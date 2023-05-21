660b80ebe39dfe7e0eccb49f4a1ce75dd9c5cfe3
nilaw2017 <nilaw.manandhar2016@gmail.com>
2023-05-21 19:44:09 +0545

    UI changes to login & register

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ router.get("/", (req, res) => {
 router.get("/login", (req, res) => {
   res.render("pages/login");
 });
+router.get("/register", (req, res)=> {
+  res.render("pages/register")
+})
 
 module.exports = router;
